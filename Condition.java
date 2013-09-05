d81428d1135d62ecdbf882ac5856a396df508c0e
Andrew Hood <andrewhood125@gmail.com>
2013-09-05 18:46:14 -0500

    Adding class Condition, controlling flow in controller on how a condition is chosen.

---
+++ Condition.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Condition here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Condition
+{
+    int numberOfTimesShown;
+    
+    public int getTimesShown()
+    {
+        return numberOfTimesShown;
+    }
+    
+    public void incrementTimesShown()
+    {
+        numberOfTimesShown++;
+    }
+}
