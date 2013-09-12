c2b7afe721abaa902428288d4bfd18d8ba13f986
Andrew Hood <andrewhood125@gmail.com>
2013-09-11 23:14:00 -0500

    Have outline for parseing program.txt when RANDOM_PRESENTATION=NO

--- DelayedConsequence.java
+++ DelayedConsequence.java
@@ -9,9 +9,30 @@ public class DelayedConsequence
 {
     public static void main(String[] args)
     {
-    	System.out.println("DEBUG: main() - making a new controller.");
+        System.out.println("DEBUG: main() - making a new controller.");
         Controller controller = new Controller();
-        System.out.println("DEBUG: main() - show instruction sheet.");
-        controller.showInstructionSheet();
+        if(Setup.getRandomPresentation())
+        {
+            System.out.println("DEBUG: main() - show instruction sheet.");
+            controller.showInstructionSheet();
+        } else {
+            while(Setup.readProgramNotComplete())
+            {
+                String line = Setup.readNextProgramLine();
+                String[] lineArray = line.split(",");
+                
+                if(lineArray[0] == "BASELINE_DVRC") { controller.showBaseline(Model.DVRC_ENUM, Integer.parseInt(lineArray[1]), Intger.parseInt(lineArray[2]));
+                if(lineArray[0] == "BASELINE_DVR") { controller.showBaseline(Model.DVR_ENUM, Integer.parseInt(lineArray[1]), Intger.parseInt(lineArray[2])); }
+                if(lineArray[0] == "BASELINE_DC") { controller.showBaseline(Model.DC_ENUM, Integer.parseInt(lineArray[1]), Intger.parseInt(lineArray[2])); }
+                if(lineArray[0] == "BASELINE_IC") { controller.showBaseline(Model.IC_ENUM, Integer.parseInt(lineArray[1]), Intger.parseInt(lineArray[2])); }}
+                if(lineArray[0] == "DVRC_SEQUENCE") { controller.beginDvrcSequence(); }
+                if(lineArray[0] == "DVR_SEQUENCE") { controller.beginDvrSequence(); }
+                if(lineArray[0] == "DC_SEQUENCE") { controller.beginDcSequence(); }
+                if(lineArray[0] == "IC_SEQUENCE") { controller.beginIcSequence(); }
+            }
+            
+            controller.gameOver();
+        }
+        
     }
 }
